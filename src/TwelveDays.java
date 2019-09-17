2b3117c8c5d310e0367e2b831740d82aa14d7ca9
li_mic000 <li_mic000@CAM-LAB-20.bgsd.k12.wa.us>
2019-09-17 08:18:34 -0700

    9/17/2019 Finished at school.

--- src/TwelveDays.java
+++ src/TwelveDays.java
@@ -2,8 +2,172 @@
 public class TwelveDays {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		day_one();
+		day_two();
+		day_three();
+		day_four();
+		day_five();
+		day_six();
+		day_seven();
+		day_eight();
+		day_nine();
+		day_ten();
+		day_eleven();
+		day_twelve();
 
+
+	}
+	
+	public static void one() {
+		System.out.println("a partridge in a pear tree.");
+	}
+	
+	public static void two() {
+		System.out.println("two turtle doves, and");
+		one();
+	}
+	
+	public static void three() {
+		System.out.println("three French hens,");
+		two();
+	}
+	
+	public static void four() {
+		System.out.println("four calling birds,");
+		three();
+	}
+	
+	public static void five() {
+		System.out.println("five golden rings,");
+		four();
+	}
+	
+	public static void six() {
+		System.out.println("six geese a-laying,");
+		five();
+	}
+	
+	public static void seven() {
+		System.out.println("seven swans a-swimming,");
+		six();
+	}
+	
+	public static void eight() {
+		System.out.println("eight maids a-milking,");
+		seven();
+	}
+	
+	public static void nine() {
+		System.out.println("nine ladies dancing,");
+		eight();
+	}
+	
+	public static void ten() {
+		System.out.println("ten lords a-leaping,");
+		nine();
+	}
+	
+	public static void eleven() {
+		System.out.println("eleven pipers piping,");
+		ten();
+	}
+	
+	public static void twelve() {
+		System.out.println("Twelve drumers drumming,");
+		eleven();
+	}
+	
+	public static void sent() {
+		System.out.println("my true love sent to me");
+	}
+	
+	public static void n_line() {
+		System.out.println("");
+	}
+	
+	public static void day_one() {
+		System.out.println("On the first day of christmas,");
+		sent();
+		one();
+		n_line();
+		
+	}
+	
+	public static void day_two() {
+		System.out.println("On the second day of Christmas,");
+		sent();
+		two();
+		n_line();
+	}
+	
+	public static void day_three() {
+		System.out.println("On the third day of Christmas,");
+		sent();
+		three();
+		n_line();
+	}
+	
+	public static void day_four() {
+		System.out.println("On the fourth day of Christmas,");
+		sent();
+		four();
+		n_line();
+	}
+	
+	public static void day_five() {
+		System.out.println("On the fifth day of Christmas,");
+		sent();
+		five();
+		n_line();
+	}
+	
+	public static void day_six() {
+		System.out.println("On the sixth day of Christmas,");
+		sent();
+		six();
+		n_line();
+	}
+	
+	public static void day_seven() {
+		System.out.println("On the seventh day of Christmas,");
+		sent();
+		seven();
+		n_line();
+	}
+	
+	public static void day_eight() {
+		System.out.println("On the eighth day of Christmas,");
+		sent();
+		eight();
+		n_line();
+	}
+	
+	public static void day_nine() {
+		System.out.println("On the ninth day of Christmas,");
+		sent();
+		nine();
+		n_line();
+	}
+	
+	public static void day_ten() {
+		System.out.println("On the tenth day of Christmas,");
+		sent();
+		ten();
+		n_line();
+	}
+	
+	public static void day_eleven() {
+		System.out.println("On the eleventh day of Christmas,");
+		sent();
+		eleven();
+		n_line();
+	}
+	
+	public static void day_twelve() {
+		System.out.println("On the twelfth day of Christmas,");
+		sent();
+		twelve();
+		n_line();
 	}
 
 }
